bc3ff4627345918b64d3f5c8ed4af7f82ff5f9f0
magballs <59237083+magballs@users.noreply.github.com>
2023-12-28 23:16:08 +0100

    Added libraries, fine-tuned rotation and various syntax errors.

--- Blender_script1.0.py
+++ Blender_script1.0.py
@@ -1,13 +1,15 @@
 import bpy
 import random
+import math
+import mathutil
 
 # Set the frame range
 start_frame = 1
 end_frame = 3600
 
 # Your objects (empty planes)
-object1 = bpy.data.objects.['empty.001']
-object2 = bpy.data.objects['empty.002']
+object1 = bpy.data.objects["empty.001"]
+object2 = bpy.data.objects["empty.002"]
 
 # Starting location and rotation
 original_loc1 = object1.location.copy()
@@ -22,20 +24,29 @@ bpy.context.scene.frame_end = end_frame
 # Random motions
 def random_motion(obj, frame, original_loc, original_rot):
     bpy.context.scene.frame_set(frame)
-    obj.location = original_loc + bpy.Vector((random.uniform(-1, 1), random.uniform(-1, 1), 0))
-    obj.rotation_euler = original_rot + bpy.Vector((0, 0, random.uniform(-180, 180)))
+    obj.location = original_loc + mathutils.Vector((random.uniform(-3, 3), random.uniform(-3, 3), 0))
+    # Convert original rotation to Quaternion
+    original_rot_quat = original_rot.to_quaternion()
+    # New rotation as Quaternion
+    delta_rotation = mathutils.Euler((0, random.uniform(-math.radians(180), math.radians(180)), 0), 'XYZ').to_quaternion()
+    # Combine rotations and convert back to Euler
+    obj.rotation_euler = (original_rot_quat * delta_rotation).to_euler
+ 
     obj.keyframe_insert(data_path="location", index=-1)
     obj.keyframe_insert(data_path="rotation_euler", index=-1)
 
 # Insert keyframes with interpolation
 def set_interpolation(obj):
-    fcurves = bpy.context.object.animation_data.action.fcurves
-    for fcurve in fcurves:
-        for kf in fcurve.keyframe_points:
-            kf.interpolation = 'LINEAR' # 'BEZIER', 'CONSTANT', 'QUARTIC', 'BOUNCE' etc
+    if obj.animation_data and obj.animation_data.action:
+        fcurves = bpy.context.object.animation_data.action.fcurves
+        for fcurve in fcurves:
+            for kf in fcurve.keyframe_points:
+                kf.interpolation = 'BEZIER' # 'BEZIER', 'CONSTANT', 'QUARTIC', 'BOUNCE' etc
+    else:
+        print(f"No animation data found for {obj.name}")
 
 # Inserting keyframes
-for f in range(start_frame, end_frame, 30): # Change 30 to 90, 180 or any other value to set keyframe intervalls
+for f in range(start_frame, end_frame, 240): # Change 30 to 90, 180 or any other value to set keyframe intervalls
     random_motion(object1, f, original_loc1, original_rot1)
     random_motion(object2, f, original_loc2, original_rot2)
 
